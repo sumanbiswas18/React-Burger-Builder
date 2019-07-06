93f19a654609e6a4aacd9579aa42905aaa99a473
sumanbiswas18 <sumanbiswas0188@gmail.com>
2019-07-07 01:50:58 +0530

    adding burger by controls & show Totalprice

---
+++ src/components/Burger/BuildControls/BuildControlsArea.js
@@ -0,0 +1,26 @@
+import React from "react";
+import cssClass from "./BuildControlsArea.css";
+import Buildcontrol from "./BuildControl/BuildControl";
+
+const controls = [
+  { lable: "Salad", type: "salad" },
+  { lable: "Bacon", type: "bacon" },
+  { lable: "Cheese", type: "cheese" },
+  { lable: "Meat", type: "meat" }
+];
+const BuildControlsArea = props => (
+  <div className={cssClass.buildControlsArea}>
+    <p>Your Burger Price {props.price.toFixed(2)}</p>
+    {controls.map(ctrl => (
+      <Buildcontrol
+        key={ctrl.lable}
+        IngredientLable={ctrl.lable}
+        added={() => props.ingredientsAdded(ctrl.type)}
+        removed={() => props.ingredientsRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
+);
+
+export default BuildControlsArea;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import Auxilary from "../../hoc/Auxilary";
 import Burger from "../../components/Burger/Burger";
+import BuildControlsArea from "../../components/Burger/BuildControls/BuildControlsArea";
+
+// setting upthe base price for each ingredients
+const PRICEING = {
+  salad: 0.4,
+  bacon: 0.6,
+  cheese: 0.8,
+  meat: 1.5
+};
 
 class BurgerBuilder extends Component {
   state = {
@@ -9,14 +18,64 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
+    },
+    totalPrice: 5
+  };
+
+  addIngredientHandler = type => {
+    // for ingredients adding
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updateIngredients = {
+      ...this.state.ingredients
+    };
+    updateIngredients[type] = updatedCount;
+
+    // for ingredients price Adding
+    const priceAddition = PRICEING[type];
+    const oldPrice = this.state.totalPrice;
+    const updatedPrice = priceAddition + oldPrice;
+    console.log(updatedPrice);
+    this.setState({ ingredients: updateIngredients, totalPrice: updatedPrice });
+  };
+  removeIngredientHandler = type => {
+    // for ingredients adding
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
     }
+    const updatedCount = oldCount - 1;
+    const updateIngredients = {
+      ...this.state.ingredients
+    };
+    updateIngredients[type] = updatedCount;
+
+    // for ingredients price Adding
+    const priceDiduection = PRICEING[type];
+    const oldPrice = this.state.totalPrice;
+    const updatedPrice = oldPrice - priceDiduection;
+    this.setState({
+      ingredients: updateIngredients,
+      totalPrice: updatedPrice
+    });
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <Auxilary>
         <Burger ingredients={this.state.ingredients} />
-        <div>buger ingredients</div>
+        <BuildControlsArea
+          ingredientsAdded={this.addIngredientHandler}
+          ingredientsRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+        />
       </Auxilary>
     );
   }
